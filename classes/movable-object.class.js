2be3bdc8627e39ea1409b6d35352c176dbf925f1
Kai Janicek <100490095+kaix6@users.noreply.github.com>
2024-06-14 12:03:44 +0200

    hitbox

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -11,8 +11,6 @@ class MovableObject {
   speedY = 0;
   acceleration = 2;
 
-
-
   applyGravity() {
     setInterval(() => {
       if (this.isAboveGround() || this.speedY > 0) {
@@ -39,6 +37,20 @@ class MovableObject {
       img.src = path;
       this.imageCache[path] = img;
     });
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    if (this instanceof Character || this instanceof Chicken || this instanceof ChickenSmall || this instanceof Endboss) {
+      ctx.beginPath();
+      ctx.lineWidth = "3";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
   }
 
   moveRightCharacter() {
